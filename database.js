a32eb7e098e6121de433cf1591f387865ed20eca
Jay Zhou <hollowic@hotmail.com>
2019-12-11 16:13:02 -0800

    Added error msgs and pages, fixed some route logic, delete override

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const urlDatabase = {
   b2xVn2: { longURL: "https://www.lighthouselabs.ca", userID: "userRandomID" },
   Gsm5xK: { longURL: "https://www.google.com", userID: "userRandomID" },
   Qf0Ri3: { longURL: "https://github.com", userID: "user2RandomID" },
-  Ar6Gy7: { longURL: "https://developer.mozilla.org", userID: "user2RandomID" },
+  Ar6Gy7: { longURL: "https://www.npmjs.com", userID: "user2RandomID" },
   b6UTxQ: { longURL: "https://developer.mozilla.org", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
   y0Nocm: { longURL: "https://www.twitch.tv", userID: "aJ48lW" },
--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
+const methodOverride = require("method-override");
 const {
   getUserByEmail,
   generateRandomString,
@@ -22,63 +23,75 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   })
 );
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
+
 //GET routes
 //Login
 app.get("/login", (req, res) => {
-  const templateVars = { username: users[req.session.user_id] };
+  const templateVars = { username: users[req.session.user_id], error: null };
   res.render("login_page", templateVars);
 });
 //Logout
 app.get("/logout", (req, res) => {
   req.session.user_id = null;
-  res.redirect(`http://localhost:8080/urls`);
+  res.redirect(`/urls`);
 });
 //Reg
 app.get("/register", (req, res) => {
-  const templateVars = { username: users[req.session.user_id] };
+  const templateVars = { username: users[req.session.user_id], error: null };
   res.render("registration_page", templateVars);
 });
 //Index page
 app.get("/urls", (req, res) => {
-  if (req.session.user_id === undefined) {
-    res.redirect("http://localhost:8080/login");
+  if (req.session.user_id === undefined || req.session.user_id === null) {
+    const templateVars = {
+      urls: urlDatabase,
+      username: users[req.session.user_id],
+      error: null
+    };
+    res.render("login_page", templateVars);
     return;
   }
-  const templateVars = {
-    urls: urlsForUser(req.session.user_id, urlDatabase),
-    username: users[req.session.user_id]
-  };
-  res.render("urls_index", templateVars);
+  if (req.session.user_id !== undefined || req.session.user_id !== null) {
+    const templateVars = {
+      urls: urlsForUser(req.session.user_id, urlDatabase),
+      username: users[req.session.user_id],
+      error: null
+    };
+    res.render("urls_index", templateVars);
+    return;
+  }
 });
 //New page
 app.get("/urls/new", (req, res) => {
-  if (req.session.user_id === undefined) {
-    res.redirect("http://localhost:8080/login");
+  if (req.session.user_id === undefined || req.session.user_id === null) {
+    res.redirect("/login");
     return;
   }
-  const templateVars = { username: users[req.session.user_id] };
+  const templateVars = { username: users[req.session.user_id], error: null };
   res.render("urls_new", templateVars);
 });
 //ShortURL page
 app.get("/urls/:shortURL", (req, res) => {
   if (req.session.user_id === undefined) {
-    res.redirect("http://localhost:8080/login");
+    res.redirect("/login");
     return;
   }
-  if (
-    urlsForUser(req.session.user_id, urlDatabase)[req.params.shortURL] ===
-    undefined
-  ) {
+  const thisUser = urlsForUser(req.session.user_id, urlDatabase)[
+    req.params.shortURL
+  ];
+  if (thisUser === undefined) {
     res
       .status(404)
-      .send("This short URL doesn't belong to you or it doesnt exist");
+      .render("404", { username: users[req.session.user_id], error: null });
     return;
   }
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: users[req.session.user_id]
+    username: users[req.session.user_id],
+    error: null
   };
   res.render("urls_show", templateVars);
 });
@@ -87,7 +100,7 @@ app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL] === undefined) {
     res
       .status(404)
-      .send("This short URL doesn't belong to you or it doesnt exist");
+      .render("404", { username: users[req.session.user_id], error: null });
     return;
   }
   const longURL = urlDatabase[req.params.shortURL].longURL;
@@ -101,24 +114,28 @@ app.post("/urls", (req, res) => {
     longURL: req.body.longURL,
     userID: req.session.user_id
   };
-  res.redirect(`http://localhost:8080/urls`);
+  res.redirect("/urls");
 });
 //Edit ShortURL method
 app.post("/urls/:shortURL", (req, res) => {
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
-    urlDatabase[req.params.shortURL] = req.body.longURL;
-    res.redirect(`http://localhost:8080/urls`);
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+    res.redirect("/urls");
   } else {
-    res.status(403).send("You're an idiot!");
+    res
+      .status(403)
+      .render("403", { username: users[req.session.user_id], error: null });
   }
 });
 //Delete method
-app.post("/urls/:shortURL/delete", (req, res) => {
+app.delete("/urls/:shortURL/delete", (req, res) => {
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
-    res.redirect(`http://localhost:8080/urls`);
+    res.redirect(`/urls`);
   } else {
-    res.status(403).send("You're an idiot!");
+    res
+      .status(403)
+      .render("403", { username: users[req.session.user_id], error: null });
   }
 });
 //Login page
@@ -128,31 +145,47 @@ app.post("/login", (req, res) => {
     getUserByPassword(req.body.password, users)
   ) {
     req.session.user_id = getUserById(req, users);
-    res.redirect(`http://localhost:8080/urls`);
+    res.redirect(`/urls`);
     return;
   }
   if (!getUserByEmail(req.body.email, users)) {
-    res.status(403).send("Email can not be found");
+    res.status(403).render("login_page", {
+      username: null,
+      error: "Wrong password or email!"
+    });
+    return;
   }
   if (
     getUserByEmail(req.body.email, users) &&
     !getUserByPassword(req.body.password, users)
   ) {
-    res.status(403).send("Wrong password");
+    res.status(403).render("login_page", {
+      username: null,
+      error: "Wrong password or email!"
+    });
+    return;
   }
 });
 //Logout method
 app.post("/logout", (req, res) => {
   req.session.user_id = null;
-  res.redirect(`http://localhost:8080/urls`);
+  res.redirect(`/urls`);
 });
 //Reg
 app.post("/register", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
-    res.status(400).send("None shall pass");
+    res.status(400).render("registration_page", {
+      username: null,
+      error: "Please enter valid values!"
+    });
+    return;
   }
   if (getUserByEmail(req.body.email, users)) {
-    res.status(400).send("Email already exist");
+    res.status(400).render("registration_page", {
+      username: null,
+      error: "Email already exist!"
+    });
+    return;
   }
   const userID = generateRandomString();
   const email = req.body.email;
@@ -163,7 +196,7 @@ app.post("/register", (req, res) => {
     password: hashedPassword
   };
   req.session.user_id = userID;
-  res.redirect(`http://localhost:8080/urls`);
+  res.redirect(`/urls`);
 });
 //Listen @ port
 app.listen(PORT, () => {
